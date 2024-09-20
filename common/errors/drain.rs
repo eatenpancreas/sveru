5b3d73d9c8144f4440ecbb55f08d4e3e3a39ce9f
Pascal d'Achard <0309597@student.rocvantwente.nl>
2024-09-20 19:29:27 +0200

    push into and regular push

--- common/errors/drain.rs
+++ common/errors/drain.rs
@@ -5,7 +5,14 @@ pub struct ErrorDrain<Err: Serialize>(Vec<Err>);
 
 impl <Err: Serialize> ErrorDrain<Err> {
   pub fn new() -> Self { Self ( vec![] )}
-  pub fn push<T, E: Into<Err>>(&mut self, res: Result<T, E>) -> Option<T> {
+  pub fn push<T>(&mut self, res: Result<T, Err>) -> Option<T> {
+    match res {
+      Ok(t) => return Some(t),
+      Err(e) => self.0.push(e)
+    }
+    None
+  }
+  pub fn push_into<T, E: Into<Err>>(&mut self, res: Result<T, E>) -> Option<T> {
     match res {
       Ok(t) => return Some(t),
       Err(e) => self.0.push(e.into())
