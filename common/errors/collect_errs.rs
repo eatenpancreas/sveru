7583a5f0f29ab6d0528bf78a6314a182872299ef
Pascal d'Achard <0309597@student.rocvantwente.nl>
2024-10-06 11:02:32 +0200

    collect errs

--- common/errors/collect_errs.rs
+++ common/errors/collect_errs.rs
@@ -4,8 +4,6 @@ pub trait CollectFirstErr<T> {
 
 impl <T, E> CollectFirstErr<Result<Vec<T>, E>> for Vec<Result<T, E>> {
   fn collect_errs(self) -> Result<Vec<T>, E> {
-    let capacity = self.capacity();
-    self.into_iter().try_fold(Vec::with_capacity(capacity), |mut acc, res|
-      res.map(|td| {acc.push(td); acc}))
+    self.into_iter().collect()
   }
 }
