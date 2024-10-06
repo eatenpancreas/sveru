6457414dcd3f7c6adc128d843754952487e3fc23
Pascal d'Achard <0309597@student.rocvantwente.nl>
2024-10-06 11:03:34 +0200

    collect errs

--- common/errors/collect_errs.rs
+++ common/errors/collect_errs.rs
@@ -1,9 +1,9 @@
 pub trait CollectFirstErr<T> {
-  fn collect_errs(self) -> T;
+  fn collect_first_err(self) -> T;
 }
 
 impl <T, E> CollectFirstErr<Result<Vec<T>, E>> for Vec<Result<T, E>> {
-  fn collect_errs(self) -> Result<Vec<T>, E> {
+  fn collect_first_err(self) -> Result<Vec<T>, E> {
     self.into_iter().collect()
   }
 }
